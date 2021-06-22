82911f415de00b716817b61ee723722a1bbbe6b2
gummicode <gummicodeblog@gmail.com>
2021-06-22 21:56:44 +0100

    Update app.js to pass the tests in the 'GET /first-characters/{string}' describe block.

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ const strings = require('./lib/strings');
 
 const app = express();
 
-app.get('/strings/hello/:entity', (req, res) => {
-  res.status(200).json({ result: strings.sayHello(req.params.entity) });
+app.get('/strings/hello/:string', (req, res) => {
+  res.status(200).json({ result: strings.sayHello(req.params.string) });
 });
 
 app.get('/strings/upper/:string', (req, res) => {
@@ -13,6 +13,14 @@ app.get('/strings/upper/:string', (req, res) => {
 
 app.get('/strings/lower/:string', (req, res) => {
   res.status(200).json({ result: strings.lowercase(req.params.string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  if (req.query.length) {
+    res.status(200).json({ result: strings.firstCharacters(req.params.string, req.query.length) });
+  } else {
+    res.status(200).json({ result: strings.firstCharacter(req.params.string) });
+  }
 });
 
 module.exports = app;
